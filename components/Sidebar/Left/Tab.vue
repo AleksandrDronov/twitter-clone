be673697ccc371880ebeaff74b396aa314923baf
Aleksandr Dronov <dronovspb@gmail.com>
2024-03-09 14:19:19 +0300

    feat: add right sidebar

--- components/Sidebar/Left/Tab.vue
+++ components/Sidebar/Left/Tab.vue
@@ -1,6 +1,6 @@
 <template>
   <nuxt-link to="#" class="flex items-center p-3 text-black w-min hover:bg-gray-200 rounded-full dark:hover:bg-dim-200 dark:text-white" :class="defaultTransition">
-    <div class="w-6 h-6 text-dark" :class="defaultTransition">
+    <div class="w-6 h-6" :class="defaultTransition">
       <slot name="icon" />
     </div>
     <div class="hidden ml-4 text-xl xl:block" :class="textClasses">
---
+++ components/Sidebar/Right/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flex flex-col">
+    <SidebarRightPreviewCard title="What's happening">
+      <SidebarRightPreviewCardItem v-for="item in whatsHappeningItems" :key="item.title">
+        <h2 class="font-bold text-gray-800 text-md dark:text-white">{{ item.title }}</h2>
+        <p class="text-gray-400 text-xs">{{ item.count }}</p>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+    <SidebarRightPreviewCard title="Who to follow">
+      <SidebarRightPreviewCardItem v-for="item in whoToFollowItems" :key="item.title">
+        <div class="flex justify-between items-center p-2">
+          <div class="flex items-center">
+            <img class="w-10 h-10 rounded-full" :src="item.image" :alt="item.name">
+            <div class="flex flex-col ml-2">
+              <h2 class="text-sm font-bold text-gray-800 dark:text-white">{{ item.name }}</h2>
+              <p class="text-xs text-gray-400">{{ item.handle }}</p>
+            </div>
+          </div>
+          <button class="px-4 py-2 text-xs font-bold text-white dark:text-black bg-black dark:bg-white rounded-full">
+            Follow
+          </button>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+  </div>
+</template>
+
+<script setup>
+
+const whatsHappeningItems = ref([
+  {
+    title: 'SpaceX',
+    count: '18.8k Tweets'
+  },
+  {
+    title: '#CodingIsFun',
+    count: '8.8k Tweets'
+  },
+  {
+    title: '#artforall',
+    count: '1.8k Tweets'
+  }
+])
+
+const whoToFollowItems = ref([
+  {
+    name: 'Elon Musk',
+    handle: '@elonmusk',
+    image: 'https://i.pravatar.cc/300'
+  },
+  {
+    name: 'Elon Musk',
+    handle: '@elonmusk',
+    image: 'https://i.pravatar.cc/300'
+  },
+  {
+    name: 'Elon Musk',
+    handle: '@elonmusk',
+    image: 'https://i.pravatar.cc/300'
+  }
+
+])
+
+</script>
